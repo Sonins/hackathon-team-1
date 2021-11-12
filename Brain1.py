cb9f902e38184f75f524055c34797be1d4828a0d
Sonins <gmlrhks95@gmail.com>
2021-11-13 03:51:34 +0900

    Added width.

--- Brain1.py
+++ Brain1.py
@@ -34,47 +34,6 @@ class Brain1:
             time.sleep(0.001)
             _ = pygame.event.get()
 
-
-            '''
-            ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
-            ☆☆☆☆☆ DO NOT CHANGE ANOTHER CODE IN 2021-Hackathon-Simulator!!! ☆☆☆☆☆
-            ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆ ONLY CHANGE Brain.py!!!☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
-            ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
-
-            1. How can I get a lidar / gps / imu data?
-                Lidar : data = self.database.lidar.data
-                Gps : data = self.database.car.position
-                IMU : data = self.database.car.direction
-
-            2. How can I move a car?
-                self.database.control.up()
-                self.database.control.down()
-                self.database.control.right()
-                self.database.control.left()
-
-                OR
-
-                self.up(num)
-                self.down(num)
-                self.right(num)
-                self.left(num)
-                ☆☆☆☆☆ num in here is number of acceleration ☆☆☆☆☆
-
-                ☆☆☆☆☆
-                In one loop,
-                you can only change the acceleration up to 5 and the angle up to 8!!
-                Maximum speed of car is 15 and maximum angle of car can rotate is 8!!
-                ☆☆☆☆☆
-
-            3. How can I get a car status data?
-                self.database.car.direction
-                self.database.car.speed
-
-            4. How can I get a v2x data?
-                self.database.v2x_data
-            '''
-
-
             if not self.database.v2x_data or not self.database.lidar.data:
                 continue
 
@@ -88,7 +47,8 @@ class Brain1:
                     traffic = {}
                     traffic['light'] = i[1]
                     traffic['position'] = i[2]
-                    traffic['remain_time'] = i[3]
+                    traffic['width'] = i[4]
+                    traffic['remain_time'] = i[5]
                     self.traffic_light.append(traffic)
 
             car_point = self.database.car.position
